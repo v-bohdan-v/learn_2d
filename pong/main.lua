0a804df587510f3f41a41e563b38ba689085ac5f
v-bohdan-v <vorobiov.bohdan@gmail.com>
2025-06-24 22:51:44 +0200

    Added moving paddles logic, start game logic and thorwing the ball logic

--- pong/main.lua
+++ pong/main.lua
@@ -5,6 +5,7 @@ require "Paddle"
 
 function love.load()
     -- INITIALIZATIONS
+    math.randomseed(os.time())
 
     -- initialize fotns
     smallFont = love.graphics.newFont('font.ttf', SMALL_FONT)
@@ -22,14 +23,45 @@ function love.load()
     playerLeftScore = 0
     playerRightScore = 0
 
+    gameState = "Start"
+
 
     love.graphics.setDefaultFilter("nearest", "nearest")
     love.graphics.setBackgroundColor(40/250, 45/250, 52/250, 0)
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, { fullscreen = false, resizable = false, vsync = false })
 end
 
-function love.update()
+function love.update(dt)
     -- MAIN LOGIC
+    -- Left player moving
+    if love.keyboard.isDown('w') then
+        playerLeft.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('s') then
+        playerLeft.dy = PADDLE_SPEED
+    else
+        playerLeft.dy = 0
+    end
+
+    -- Right player moving
+    if love.keyboard.isDown("up") then
+        playerRight.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown("down") then
+        playerRight.dy = PADDLE_SPEED
+    else
+        playerRight.dy = 0
+    end
+
+    -- Game start and throw the ball
+    if love.keyboard.isDown('return') and gameState == "Start" then
+        gameState = "Play"
+        ball.dy = math.random(-220, 220) * 5
+        ball.dx = math.random(-200, 200) * 5
+    end
+
+    -- update objects
+    playerLeft:update(dt)
+    playerRight:update(dt)
+    ball:update(dt)
 end
 
 function love.draw()
