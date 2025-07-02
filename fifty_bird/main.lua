758dc2186cb82176e3da7c54d0f70dc7a5d4f21f
v-bohdan-v <vorobiov.bohdan@gmail.com>
2025-07-02 14:15:49 +0200

    Scale picture to the screen, and add scrolling it in the correct way

--- fifty_bird/main.lua
+++ fifty_bird/main.lua
@@ -17,12 +17,28 @@ end
 
 function love.update(dt)
     background_scroll = (background_scroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-    ground_scroll = (ground_scroll + GROUND_SCROLL_SPEED * dt) % WINDOW_WIDTH
+    ground_scroll = (ground_scroll + GROUND_SCROLL_SPEED * dt) % (ground:getWidth() / 1.5)
 end
 
 function love.draw()
-    love.graphics.draw(background, -background_scroll, 0, 0, WINDOW_WIDTH / background:getWidth(), WINDOW_HEIGHT / background:getHeight())
-    love.graphics.draw(ground, -ground_scroll, WINDOW_HEIGHT - 16)
+    love.graphics.draw(
+        background, -- image
+        -background_scroll, -- x position, each frame goes to minus, so it seems like the picture goes left
+        0, -- y position, stable
+        0, -- rotation
+        3, -- scale on x axis, width * 3
+        2.5 -- scale on y axis, height * 2.5
+    )
+    love.graphics.draw(
+        ground, -- image
+        -ground_scroll, -- x position, each frame goes to minus, so it seems like the picture goes left
+        WINDOW_HEIGHT - ground:getHeight() * 2, -- y position, stable
+        0, -- rotation
+        2 -- scale on x axis, width * 6
+        -- 1, -- scale on y axis, hegith * 1
+        -- ground:getWidth() / 2, -- ox: x-axis origin offset
+        -- 0 -- oy: without offset
+    )
 end
 
 function love.keypressed()
